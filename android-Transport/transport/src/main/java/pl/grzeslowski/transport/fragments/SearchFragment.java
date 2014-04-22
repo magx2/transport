6960348b38e5bca2f5a77480f5865d403301fe49
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2014-04-22 20:51:48 +0200

    integrated search Fragment to main activity

--- android-Transport/transport/src/main/java/pl/grzeslowski/transport/fragments/SearchFragment.java
+++ android-Transport/transport/src/main/java/pl/grzeslowski/transport/fragments/SearchFragment.java
@@ -1,12 +1,47 @@
 package pl.grzeslowski.transport.fragments;
 
+import android.support.v4.app.Fragment;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.Bean;
+import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EFragment;
+import org.androidannotations.annotations.ViewById;
+
+import java.util.List;
 
 import pl.grzeslowski.transport.R;
+import pl.grzeslowski.transport.repository.DatabaseManager;
 
 /**
  * Created by Martin on 2014-04-22.
  */
 @EFragment(R.layout.fragment_search)
-public class SearchFragment {
+public class SearchFragment extends Fragment{
+    @ViewById(R.id.fragment_search_from)
+    Spinner mFrom;
+    @ViewById(R.id.fragment_search_to)
+    Spinner mTo;
+    @Bean
+    DatabaseManager mDatabaseManager;
+
+    @AfterViews
+    void prepare() {
+        List<String> names = mDatabaseManager.getCitiesNames();
+
+        ArrayAdapter fromAdapter = new ArrayAdapter(getActivity(), android.R.layout.simple_spinner_item, names);
+        mFrom.setAdapter(fromAdapter);
+
+        ArrayAdapter toAdapter = new ArrayAdapter(getActivity(), android.R.layout.simple_spinner_item, names);
+        mTo.setAdapter(toAdapter);
+
+        // mTo cannot have the same chosen item so changing it to second element
+        mTo.setSelection(1, false);
+    }
+
+    @Click(R.id.fragment_search_search)
+    void search() {}
+
 }
