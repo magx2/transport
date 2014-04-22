0b900f234ecdc7dd5785a5fe269a75fdfba3f400
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2014-04-22 21:35:53 +0200

    ResultFragment shows all (not filtering) connections when ask for find connections

--- android-Transport/transport/src/main/java/pl/grzeslowski/transport/fragments/ResultFragment.java
+++ android-Transport/transport/src/main/java/pl/grzeslowski/transport/fragments/ResultFragment.java
@@ -4,7 +4,9 @@ import android.support.v4.app.Fragment;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
+
+import com.google.common.base.Function;
+import com.google.common.collect.Lists;
 
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.Bean;
@@ -32,14 +34,27 @@ public class ResultFragment extends Fragment {
 
     @AfterViews
     void prepare() {
-        ListAdapter adapter = new ArrayAdapter(getActivity(), android.R.layout.simple_list_item_1, Arrays.asList("as", "df", "fg"));
-        mResultList.setAdapter(adapter);
+        setAdapter(Arrays.asList("as", "df", "fg"));
     }
 
     public void showResultsFor(City from, City to) {
         List<Connection> connections = mDatabaseManager.getConnections(from, to);
 
-        // TODO: Mock
-        Toast.makeText(getActivity(), String.format("from %s,%nto %s", from.getName(), to.getName()), Toast.LENGTH_SHORT).show();
+        List<String> forAdapter = Lists.transform(connections, new Function<Connection, String>() {
+            @Override
+            public String apply(Connection input) {
+                City from = input.getPath().get(0);
+                City to = input.getPath().get(input.getPath().size() - 1);
+
+                return String.format("from: %s%nto: %s%nby: %s%ntime: %s:%s", from.getName(), to.getName(), input.getProvider().getName(), input.getTime().getHourOfDay(), input.getTime().getMinuteOfHour());
+            }
+        });
+
+        setAdapter(forAdapter);
+    }
+
+    private void setAdapter(List<?> elements) {
+        ListAdapter adapter = new ArrayAdapter(getActivity(), android.R.layout.simple_list_item_1, elements);
+        mResultList.setAdapter(adapter);
     }
 }
--- android-Transport/transport/src/main/java/pl/grzeslowski/transport/repository/DatabaseManager.java
+++ android-Transport/transport/src/main/java/pl/grzeslowski/transport/repository/DatabaseManager.java
@@ -7,13 +7,15 @@ import com.google.common.collect.Iterables;
 import com.google.common.collect.Lists;
 
 import org.androidannotations.annotations.EBean;
+import org.joda.time.LocalTime;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 
 import pl.grzeslowski.transport.model.City;
 import pl.grzeslowski.transport.model.Connection;
+import pl.grzeslowski.transport.model.Provider;
 
 /**
  * Created by Martin on 2014-04-17.
@@ -21,8 +23,24 @@ import pl.grzeslowski.transport.model.Connection;
 @EBean
 public class DatabaseManager {
 
+    private static List<City> sCities = Arrays.asList(new City(1, "Swidnica"), new City(2, "Wrocław"), new City(3, "Wałbrzych"));
+    private static List<Provider> sProviders = Arrays.asList(new Provider(1, "P.W.H.D"), new Provider(2, "Guliwer"), new Provider(3, "Kolej wąskotorowa"));
+    private static List<Connection> sConnections = Arrays.asList(
+            new Connection(Arrays.asList(sCities.get(0), sCities.get(1)), getRandomLocalTime(), sProviders.get(0)),
+            new Connection(Arrays.asList(sCities.get(0), sCities.get(2)), getRandomLocalTime(), sProviders.get(1)),
+            new Connection(Arrays.asList(sCities.get(2), sCities.get(1)), getRandomLocalTime(), sProviders.get(2)),
+
+            new Connection(Arrays.asList(sCities.get(1), sCities.get(0)), getRandomLocalTime(), sProviders.get(0)),
+            new Connection(Arrays.asList(sCities.get(2), sCities.get(0)), getRandomLocalTime(), sProviders.get(1)),
+            new Connection(Arrays.asList(sCities.get(1), sCities.get(2)), getRandomLocalTime(), sProviders.get(2))
+    );
+    private static LocalTime getRandomLocalTime() {
+        Random random = new Random();
+        return new LocalTime(random.nextInt(23), random.nextInt(59));
+    }
+
     public List<City> getCities() {
-        return Arrays.asList(new City(1, "Swidnica"), new City(2, "Wrocław"), new City(3, "Wałbrzych"));
+        return sCities;
     }
 
     public List<String> getCitiesNames() {
@@ -35,7 +53,7 @@ public class DatabaseManager {
     }
 
     public List<Connection> getConnections(final City from, final City to) {
-        return new ArrayList<Connection>();
+        return sConnections;
     }
 
     public City getCityByName(final String cityName) {
