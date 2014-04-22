f1e67a4f95ca8635ba8bf1d56f569106a3043f29
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2014-04-22 20:34:44 +0200

    adding SearchFragment

---
+++ android-Transport/transport/src/main/java/pl/grzeslowski/transport/fragments/SearchFragment.java
@@ -0,0 +1,12 @@
+package pl.grzeslowski.transport.fragments;
+
+import org.androidannotations.annotations.EFragment;
+
+import pl.grzeslowski.transport.R;
+
+/**
+ * Created by Martin on 2014-04-22.
+ */
+@EFragment(R.layout.fragment_search)
+public class SearchFragment {
+}
