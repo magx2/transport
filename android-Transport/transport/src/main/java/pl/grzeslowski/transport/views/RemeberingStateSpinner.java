6427432c8da406924a305cfc8564231bb907f509
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2014-04-24 18:46:26 +0200

    added spinner that remebers its old state while changing state

---
+++ android-Transport/transport/src/main/java/pl/grzeslowski/transport/views/RemeberingStateSpinner.java
@@ -0,0 +1,61 @@
+package pl.grzeslowski.transport.views;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+
+public class RemeberingStateSpinner extends Spinner {
+    private Object mActualItem;
+    private OnItemSelected mOnItemSelected;
+
+    public RemeberingStateSpinner(Context context) {
+        super(context);
+        init();
+    }
+
+    public RemeberingStateSpinner(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public RemeberingStateSpinner(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init();
+    }
+
+    private final void init() {
+        super.setOnItemSelectedListener(new OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                RemeberingStateSpinner.this.onItemSelected(position);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+    }
+
+    private void onItemSelected(int position) {
+        mOnItemSelected.selected(position, mActualItem);
+
+        mActualItem = this.getAdapter().getItem(position);
+    }
+
+    public void setOnItemSelected(OnItemSelected onItemSelected) {
+        mOnItemSelected = onItemSelected;
+    }
+
+    @Override
+    public void setOnItemSelectedListener(OnItemSelectedListener listener) {
+        throw new UnsupportedOperationException("This is not supported! Use setOnItemSelected(...) instead.");
+    }
+
+
+    public interface OnItemSelected {
+        void selected(int newPosition, Object oldObject);
+    }
+}
