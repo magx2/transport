98f7bf67003e9ff8d780aaded94283b5b17d0ef0
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2014-05-05 19:59:23 +0200

    added creashlitics

--- android-Transport/transport/src/main/java/pl/grzeslowski/transport/activities/MainActivity.java
+++ android-Transport/transport/src/main/java/pl/grzeslowski/transport/activities/MainActivity.java
@@ -7,13 +7,23 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.crashlytics.android.Crashlytics;
+
 import org.androidannotations.annotations.EActivity;
 
 import pl.grzeslowski.transport.R;
 
 
+
 @EActivity(R.layout.activity_main)
 public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Crashlytics.start(this);
+    }
 
     /**
      * A placeholder fragment containing a simple view.
