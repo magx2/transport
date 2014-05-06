4856a8e2509b9c6ea47a0415d6c37b5b9840ebac
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2014-05-07 00:05:52 +0200

    added ad sense to ResultActivity

--- android-Transport/transport/src/main/java/pl/grzeslowski/transport/activities/ResultActivity.java
+++ android-Transport/transport/src/main/java/pl/grzeslowski/transport/activities/ResultActivity.java
@@ -2,6 +2,8 @@ package pl.grzeslowski.transport.activities;
 
 import android.support.v7.app.ActionBarActivity;
 
+import com.google.android.gms.analytics.HitBuilders;
+import com.google.android.gms.analytics.Tracker;
 import com.google.common.base.Preconditions;
 
 import org.androidannotations.annotations.AfterViews;
@@ -11,6 +13,7 @@ import org.androidannotations.annotations.FragmentById;
 import org.androidannotations.annotations.OptionsItem;
 
 import pl.grzeslowski.transport.R;
+import pl.grzeslowski.transport.TransporterApplication;
 import pl.grzeslowski.transport.fragments.ResultFragment;
 import pl.grzeslowski.transport.model.City;
 
@@ -33,10 +36,26 @@ public class ResultActivity extends ActionBarActivity {
 
         // turning on back method for action bar
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        initAdSense();
     }
 
     @OptionsItem(android.R.id.home)
     void multipleMenuItems() {
         finish();
+    }
+
+    private void initAdSense() {
+
+        // Get tracker.
+        final TransporterApplication application = (TransporterApplication) getApplication();
+        Tracker t = application.getTracker();
+
+        // Set screen name.
+        // Where path is a String representing the screen name.
+        t.setScreenName(ResultActivity.class.getCanonicalName());
+
+        // Send a screen view.
+        t.send(new HitBuilders.AppViewBuilder().build());
     }
 }
