26773ba38abaed2452954d81381bce41c771b6e7
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2014-05-06 23:37:25 +0200

    added Application class

---
+++ android-Transport/transport/src/main/java/pl/grzeslowski/transport/TransporterApplication.java
@@ -0,0 +1,9 @@
+package pl.grzeslowski.transport;
+
+import android.app.Application;
+
+import org.androidannotations.annotations.EApplication;
+
+@EApplication
+public class TransporterApplication extends Application {
+}
